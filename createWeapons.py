186c326f1d80107ab3ff3aaaa93c5d3602d6089b
Chr Isso <csodl@web.de>
2024-04-26 18:43:50 +0200

    SomeChanges

--- createWeapons.py
+++ createWeapons.py
@@ -4,19 +4,18 @@ import string
 from mwcleric import AuthCredentials
 from mwcleric import WikiggClient
 
-WIKITEXT = """{{{{Item infobox
-|Name={Name}
+WIKITEXT = """{{{{Weapon Infobox
 |Research={Research}
 |Type={Type}
-|Fire_Mode={Fire_Mode}
+|FireMode={FireMode}
 |Damage={Damage}
-|Stun_Power={Stun_Power}
-|Hipfire_Range={Hipfire_Range}
-|ADS_Range={ADS_Range}
+|StunPower={StunPower}
+|HipfireRange={HipfireRange}
+|ADSRange={ADSRange}
 |Stability={Stability}
-|Rate_of_Fire={Rate_of_Fire}
+|RateOfFire={RateOfFire}
 |Mobility={Mobility}
-|Clip_Size={Clip_Size}
+|ClipSize={ClipSize}
 |Lore={Lore}
 |Description={Description}
 }}}}"""
@@ -25,24 +24,23 @@ WIKITEXT = """{{{{Item infobox
 class Creator:
     def __init__(self):
         credentials = AuthCredentials(user_file="me")
-        self.site = WikiggClient('witchfire.wiki.gg', credentials=credentials)
-        self.summary = 'Tabber -> Gallery'
+        self.site = WikiggClient('witchfire', credentials=credentials)
+        #self.summary = 'Tabber -> Gallery'
         with open('weapons.json', 'r', encoding='utf-8') as f:
             self.data = json.load(f)
 
     def run(self):
         for k, v in self.data.items():
             self.site.client.pages[string.capwords(k)].save(WIKITEXT.format(
-                Name =v['Research'],
                 Research=v['Research'],
                 Type=v['Type'],              
-                Fire_Mode=v['Fire_Mod'], 
+                FireMode=v['Fire_Mode'], 
                 Damage=v['Damage'], 
-                Stun_Power=v['Stun_Power'], 
-                Hipfire_Range=v['Hipfire_Range'], 
-                ADS_Range=v['ADS_Range'], 
-                Stability=v['tability'], 
-                Rate_of_Fire=v['Rate_of_Fire'], 
+                StunPower=v['Stun_Power'], 
+                HipfireRange=v['Hipfire_Range'], 
+                ADSRange=v['ADS_Range'], 
+                Stability=v['Stability'], 
+                RateOfFire=v['Rate_of_Fire'], 
                 Mobility=v['Mobility'], 
                 Clip_Size=v['Clip_Size'], 
                 Lore=v['Lore'], 
