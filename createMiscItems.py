49e60b527d14a712cac03492c8e65ea3fbb1aaf6
Chr Isso <csodl@web.de>
2024-06-02 11:37:09 +0200

    MiscItems

---
+++ createMiscItems.py
@@ -0,0 +1,43 @@
+import json
+import string
+
+from mwcleric import AuthCredentials
+from mwcleric import WikiggClient
+
+WIKITEXT = """{{{{MiscItem Infobox
+|Name={Name}
+|Type={Type}
+|Description={Description}
+|Lore={Lore}
+|Stash={Stash}
+|Purchasable={Purchasable}
+|Price={Price}
+|Worth={Worth}
+}}}}"""
+
+
+class Creator:
+    def __init__(self):
+        credentials = AuthCredentials(user_file="me")
+        self.site = WikiggClient('witchfire', credentials=credentials)
+        #self.summary = 'Tabber -> Gallery'
+        with open('MiscItem.json', 'r', encoding='utf-8') as f:
+            self.data = json.load(f)
+
+    def run(self):
+        PAGETEXT = ''
+        for k, v in self.data.items():
+            PAGETEXT = PAGETEXT + WIKITEXT.format(
+                Name=k,           
+                Type=v['Type'],
+                Description=v['Description'],
+                Lore=v['Lore'],
+                Stash=v['Stash'],
+                Purchasable=v['Purchasable'],
+                Price=v['Price'],
+                Worth=v['Worth']
+            )
+        self.site.client.pages['MiscItem Infobox'].save(PAGETEXT)
+
+if __name__ == '__main__':
+    Creator().run()
