87ef77e5a3f710e497ddef19f38a0acf245bbebe
Chr Isso <csodl@web.de>
2024-09-23 15:54:16 +0200

    bugfix

--- purgeCacheForAllPages.py
+++ purgeCacheForAllPages.py
@@ -9,6 +9,8 @@ class Creator:
         credentials = AuthCredentials(user_file="me")
         self.site = WikiggClient('witchfire', credentials=credentials)
         #self.summary = 'Tabber -> Gallery'
+
+    def run(self):
         for k in self.site.client.allpages(namespace=0):
             self.site.purge(k)
             print('Purging: ',k)
